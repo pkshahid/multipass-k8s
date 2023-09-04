ae8c13bb7e0ced7dd33a73dbf6bec81a8ac1c57f
Shahid@Galaxy <shahidmohammedmsp@gmail.com>
2023-09-04 23:09:17 +0530

    Added Prompt

--- launch.sh
+++ launch.sh
@@ -1,3 +1,20 @@
-multipass launch --name master -c 2 -m 2G --disk 10G
-multipass launch --name worker -c 2 -m 2G --disk 10G
+# multipass launch --name master -c 2 -m 2G --disk 10G
+# multipass launch --name worker -c 2 -m 2G --disk 10G
 
+#!/bin/bash
+
+# Prompt user for the values of -c, -m, and --disk for master node
+read -p "Enter the Number of CPUs of Master Node VM: " cpus
+read -p "Enter the value for RAM of Master Node VM (in GB): " memory
+read -p "Enter the value for Disk Space of Master Node VM (in GB): " disk
+
+# Launch the multipass instance with user-provided values
+multipass launch --name master -c "$cpus" -m "$memory"G --disk "$disk"G
+
+# Prompt user for the values of -c, -m, and --disk for master node
+read -p "Enter the Number of CPUs of Worker Node VM: " cpus
+read -p "Enter the value for RAM of Worker Node VM (in GB): " memory
+read -p "Enter the value for Disk Space of Worker Node VM (in GB): " disk
+
+# Launch the multipass instance with user-provided values
+multipass launch --name worker -c "$cpus" -m "$memory"G --disk "$disk"G
